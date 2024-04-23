d9f0a5d770bed30012da89111b3bebc8db4ce013
sergee1611 <167037492+sergee1611@users.noreply.github.com>
2024-04-23 21:22:01 +0300

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,13 @@
+fruits = ['apple', 'orange', 'banana', 'pineapple', 'cherry']
+print(fruits)
+print(fruits[0], fruits[-1])
+print(fruits[2:6])
+fruits[2] = 'lemon'
+print(fruits)
+
+my_dict = {'apple': 'яблоко', 'orange': 'апельсин', 'lemon': 'лимон', 'pineapple': 'ананас'}
+print(my_dict)
+print(my_dict['orange'])
+my_dict['apple'] = 'не яблоко'
+my_dict['mango'] = 'манго'
+print(my_dict)
